32b31d89e22d4b48170cc277fc1c14d61920732a
vesperdiesel <1850594465@qq.com>
2019-06-21 17:22:45 +0800

    添加home和login组件以及配置路由导航

---
+++ src/views/home/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'AppHome',
+  props: [''],
+  data () {
+    return {
+
+    }
+  },
+
+  components: {},
+
+  computed: {},
+
+  beforeMount () {},
+
+  mounted () {},
+
+  methods: {},
+
+  watch: {}
+
+}
+
+</script>
+<style lang='less' scoped>
+
+</style>
---
+++ src/views/login/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'AppLogin',
+  props: [''],
+  data () {
+    return {
+
+    }
+  },
+
+  components: {},
+
+  computed: {},
+
+  beforeMount () {},
+
+  mounted () {},
+
+  methods: {},
+
+  watch: {}
+
+}
+
+</script>
+<style lang='less' scoped>
+
+</style>
