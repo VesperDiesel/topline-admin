c5f1b5126f9b6999f4c9819cd04144a8fcc4f746
vesperdiesel <1850594465@qq.com>
2019-06-24 19:56:50 +0800

    退出登录

--- src/views/layout/components/AppHeader.vue
+++ src/views/layout/components/AppHeader.vue
@@ -2,14 +2,14 @@
  <el-row :gutter="20">
     <el-col :span="10">江苏传智播客教育科技股份有限公司</el-col>
     <el-col :span="5" :offset="5">
-      <el-dropdown>
-        <img width="30" src="userInfo.photo">
+      <el-dropdown trigger="click" @command='handleCommand'>
         <span class="el-dropdown-link">
+          <img width="30" src="userInfo.photo">
           {{userInfo.name}}<i class="el-icon-arrow-down el-icon--right"></i>
         </span>
         <el-dropdown-menu slot="dropdown">
-          <el-dropdown-item>用户设置</el-dropdown-item>
-          <el-dropdown-item>退出</el-dropdown-item>
+          <el-dropdown-item command='a'>用户设置</el-dropdown-item>
+          <el-dropdown-item @click.native="handleLogout">退出</el-dropdown-item>
         </el-dropdown-menu>
       </el-dropdown>
     </el-col>
@@ -37,12 +37,32 @@ export default {
 
   mounted () {},
 
-  methods: {},
-
-  watch: {}
-
+  methods: {
+    handleLogout () {
+      this.$confirm('确认退出么？', '退出提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(() => {
+        window.localStorage.removeItem('user_info')
+        this.$router.push({ name: 'login' })
+        this.$message({
+          type: 'success',
+          message: '退出成功!'
+        })
+      }).catch(() => {
+        this.$message({
+          type: 'info',
+          message: '已取消退出'
+        })
+      })
+    },
+    handleCommand (dropdownItem) {
+      console.log('handleCommand', dropdownItem)
+    }
+  }
 }
-
 </script>
+
 <style lang='' scoped>
 </style>
