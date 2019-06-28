93222c09baa505ce027ac3422d43bcb7be33a458
vesperdiesel <1850594465@qq.com>
2019-06-28 12:01:32 +0800

    筛选功能完成

--- src/views/article/index.vue
+++ src/views/article/index.vue
@@ -34,7 +34,7 @@
           </el-date-picker>
         </el-form-item>
         <el-form-item>
-          <el-button type="primary" @click="onSubmit">查询</el-button>
+          <el-button type="primary" @click="handleFilter" :loading='articleLoading'>查询</el-button>
         </el-form-item>
       </el-form>
     </el-card>
@@ -43,7 +43,7 @@
     <!-- 文章列表 -->
     <el-card class="box-card">
       <div slot="header" class="clearfix">
-        <span>一共有xxx条数据</span>
+        <span>一共有 <strong>{{ totalCount }}</strong> 条数据</span>
         <el-button style="float: right; padding: 3px 0" type="text">操作按钮</el-button>
       </div>
       <el-table
@@ -91,9 +91,10 @@
         </el-table-column>
       </el-table>
 
-      <!-- 数据分页 -->
+      <!-- 数据分页  current-page当前高亮页,需要于数据保持同步-->
       <el-pagination
         background
+        :current-page='page'
         layout="prev, pager, next"
         :pagesize="pageSize"
         :total='totalCount'
@@ -181,14 +182,26 @@ export default {
         this.$message.err('获取频道数据失败')
       }
     },
-    onSubmit () {},
+    handleFilter () {
+      this.page = 1
+      this.loadArticles()
+    },
     async loadArticles () {
+      this.articleLoading = true
+      const filterData = {}
+      for (let key in this.filterParams) {
+        const item = this.filterParams[key]
+        if (item !== null && item !== undefined && item !== '') {
+          filterData[key] = item
+        }
+      }
       const data = await this.$http({
         method: 'GET',
         url: '/articles',
         params: {
           page: this.page,
-          per_page: this.pageSize
+          per_page: this.pageSize,
+          ...filterData
         }
       })
       this.articles = data.results
