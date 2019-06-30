15eb16593c96281dd31a2cbd63783a95c2846a7b
vesperdiesel <1850594465@qq.com>
2019-06-30 22:20:46 +0800

    禁用路由缓存，防止使用相同组件，跳转路由组件不重新加载

--- src/views/layout/index.vue
+++ src/views/layout/index.vue
@@ -11,7 +11,17 @@
       </el-header>
       <!-- 内容 -->
       <el-main>
-        <router-view></router-view>
+        <router-view :key='$route.path'>
+          <!--
+          禁用路由缓存，只需要给 router-view 添加一个唯一的 key 即可
+          这样路由就不会去缓存组件了
+          这个 key 要确保唯一
+          禁用路由换粗不是必须的，看情况
+          如果你要刻意使用默认的路由缓存能力，那么就不要禁用，去呗缓存的路由中通过
+            方式一：监视 $route 做处理
+            方式二：2.2 新增的 beforeRouteUpdate 路由导航钩子做处理
+         -->
+        </router-view>
       </el-main>
     </el-container>
   </el-container>
